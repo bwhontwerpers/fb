27a3fff5395a85fe8584f75bf899c76a4c9f6fb1
BWH ontwerpers <interactief@bwhontwerpers.nl>
2018-05-04 17:24:23 +0200

    Update fb.js

--- fb.js
+++ fb.js
@@ -1,20 +1,5 @@
 jQuery( document ).ready(function( $ ) {
  
- var url = 'http://www.2cvgarage.nl';
- var apiUrl = 'https://graph.facebook.com/?ids='+url;
- 
- $.ajax({url: apiUrl, success: function(result){
- 
-        $.each( result, function( key, val ) {
- 
-        	console.log(key + ' - ' + val['share']['share_count']);
- 
-        	var shareCount = val['share']['share_count'];
-        	
-        	$(".facebook_counter").html(shareCount);
- 
-   });
- 
- }});
+$(".facebook_counter").html('0');
 
 });
