68dfd3d939d879cdc4ae6de72b72703119f887bf
BWH ontwerpers <interactief@bwhontwerpers.nl>
2018-05-17 16:06:31 +0200

    Update fb.js

--- fb.js
+++ fb.js
@@ -1,5 +1,7 @@
 jQuery( document ).ready(function( $ ) {
  
-$(".facebook_counter").html('0');
+	var e = $(".facebook_counter").attr("data-fb");
+	var fblike_url = "https://graph.facebook.com/v3.0/"+e+"?fields=new_like_count%2Cfan_count&access_token=1039597392765579%7CNw9h9B7_9Psd7rqz5mEUPOUcz2Y";
+	$.ajax({type:"GET",url:fblike_url,dataType:"jsonp",success:function(e){$(".facebook_counter").html(e.fan_count);}});
 
 });
